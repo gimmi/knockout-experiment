e06e090d6239463ab6f159185a4a2b4fd5b7297d
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-12-29 19:21:29 +0100

    tree

--- src/Trackr/js/app.js
+++ src/Trackr/js/app.js
@@ -1,6 +1,6 @@
 trackr = { };
 
-trackr.AppController = function (server) {
+trackr.AppController = function(server) {
 	this._server = server;
 
 	this.nodes = ko.observableArray();
@@ -9,24 +9,24 @@ trackr.AppController = function (server) {
 };
 
 trackr.AppController.prototype = {
-	loadTree: function () {
-		this._server.call('getTree', function (datas) {
-			this.nodes(_(datas).map(function (data) {
+	loadTree: function() {
+		this._server.call('getTree', function(datas) {
+			this.nodes(_(datas).map(function(data) {
 				return new trackr.TreeNodeViewModel(data);
 			}, this));
 		}, this);
 	}
 };
 
-trackr.TreeNodeViewModel = function (data) {
+trackr.TreeNodeViewModel = function(data) {
 	this.text = ko.observable(data.text);
 	this.expanded = ko.observable(false);
-	this.children = ko.observableArray(_(data.children).map(function (child) {
-		return new trackr.TreeNodeViewModel(child);
-	}));
+	this.children = ko.observableArray(_(data.children).map(function(child) { return new trackr.TreeNodeViewModel(child); }));
+	this.isLeaf = ko.computed(function() { return this.children().length === 0; }, this);
 };
+
 trackr.TreeNodeViewModel.prototype = {
-	click: function () {
+	click: function() {
 		this.expanded(!this.expanded());
 	}
 };
