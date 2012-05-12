719861c02f2cfc1261683399284ef5864b8e1435
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2012-05-12 12:40:15 +0200

    Removed ScrollBindingHandler and Crossroads configuration

--- client/js/main.js
+++ client/js/main.js
@@ -1,33 +1,11 @@
 define(function (require) {
 	var ko = require('knockout'),
 		AppController = require('AppController'),
-		BottomScrollBindingHandler = require('BottomScrollBindingHandler'),
-		StringTemplateEngine = require('StringTemplateEngine'),
-		crossroads = require('crossroads'),
-		hasher = require('hasher');
-
-	ko.bindingHandlers.bottomScroll = new BottomScrollBindingHandler();
+		StringTemplateEngine = require('StringTemplateEngine');
 
 	ko.setTemplateEngine(new StringTemplateEngine());
 
 	var appController = new AppController();
 	appController.loadTree();
 	ko.applyBindings(appController);
-
-	crossroads.addRoute('tasks', function () {
-		console.log('tasks');
-	});
-	crossroads.addRoute('milestones', function () {
-		console.log('milestones');
-	});
-	if (!hasher.getHash()) {
-		hasher.setHash('tasks');
-	}
-	hasher.initialized.add(function (newHash) {
-		crossroads.parse(newHash);
-	});
-	hasher.changed.add(function (newHash) {
-		crossroads.parse(newHash);
-	});
-	hasher.init();
 });
